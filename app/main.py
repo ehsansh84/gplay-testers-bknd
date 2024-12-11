4e885da01469cd466e3b34d7d13af6ea4f6656d9
ehsanyl <163835521+ehsanyl@users.noreply.github.com>
2024-12-12 00:31:24 +0100

    removed: unused routes

--- app/main.py
+++ app/main.py
@@ -7,23 +7,10 @@ from fastapi.middleware.cors import CORSMiddleware
 project_dir = os.getenv("PROJECT_DIR")
 if project_dir:
     sys.path.append(project_dir)
-from app.api.v1.user import router as user_router
-from app.api.v1.content import router as content_router
-from app.api.v1.category import router as category_router
-from app.api.v1.privacy import router as privacy_router
-from app.api.v1.health_check import router as health_check_router
-from app.api.v1.like import router as like_router
-from app.api.v1.share import router as share_router
-from app.api.v1.theme import router as theme_router
-from app.api.v1.visit import router as visit_router
-from app.api.v1.setting import router as setting_router
-from app.api.v1.reference import router as reference_router
-from app.api.v1.shop import router as shop_router
-from app.api.v1.subscribe import router as subscribe_router
-from app.api.v1.our_apps import router as our_apps_router
+from app.api.v1.test_log import router as test_logrouter
 
 sentry_sdk.init(
-    dsn="https://f8dde9c4865811efb51c0242ac12002b@sentry.pixanio.com/6",
+    dsn="https://f42ecd13ecf3fd56818667660e2c0a52@sentry.pixanio.com/9",
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for tracing.
     traces_sample_rate=1.0,
@@ -52,22 +39,8 @@ app.add_middleware(
 async def trigger_error():
     division_by_zero = 1 / 0
 
-app.include_router(router=user_router)
-app.include_router(router=content_router)
-app.include_router(router=category_router)
-app.include_router(router=privacy_router)
-app.include_router(router=health_check_router)
-app.include_router(router=like_router)
-app.include_router(router=share_router)
-app.include_router(router=theme_router)
-app.include_router(router=visit_router)
-app.include_router(router=setting_router)
-app.include_router(router=reference_router)
-app.include_router(router=shop_router)
-app.include_router(router=subscribe_router)
-app.include_router(router=our_apps_router)
+app.include_router(router=test_logrouter)
 app.mount("/static", StaticFiles(directory='static'), name="static")
-# app.mount("/uploaded", StaticFiles(directory=Settings.UPLOAD_PATH), name="uploaded")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8200, reload=True)
